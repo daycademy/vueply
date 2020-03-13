febbebcf2541ac4363dbdb6aafcd695a3ba459fe
florianwoelki <florianwoelki@gmx.de>
2020-03-13 15:23:50 +0100

    refactor(component): refactor code from home view to extra preview component
    This component will have the resulting compiled vue code.

---
+++ src/components/Preview.vue
@@ -0,0 +1,69 @@
+<template>
+  <iframe
+    id="preview"
+    name="result"
+    sandbox="allow-forms allow-popups allow-scripts allow-same-origin"
+    frameborder="0"
+  ></iframe>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    templateCode: String,
+    jsCode: String,
+  },
+
+  mounted() {
+    this.showPreview();
+  },
+
+  watch: {
+    templateCode() {
+      this.showPreview();
+    },
+    jsCode() {
+      this.showPreview();
+    },
+  },
+
+  methods: {
+    showPreview() {
+      const lib = 'https://cdn.jsdelivr.net/npm/vue@2.6.11';
+      const templateCode = this.templateCode.replace(/\s*\n+\s*/g, ' ').replace(/>\s+/g, '>').replace(/\s+</g, '<');
+      const { jsCode } = this;
+      /* eslint-disable-next-line */
+      const script = 'var template = `<template>' + templateCode + '</template>`;' + 'var js =`' + jsCode + '`;';
+
+      const previewDoc = window.frames[0].document;
+      previewDoc.write('<!DOCTYPE html>');
+      previewDoc.write('<html>');
+      previewDoc.write('<head>');
+      previewDoc.write(`<script src=${lib} type="text/javascript"><\/script>`);
+      previewDoc.write('</head>');
+      previewDoc.write('<body>');
+      previewDoc.write('<div id="app"></div>');
+      previewDoc.write(`<script type="text/javascript">${script}<\/script>`);
+      previewDoc.write(`<script type="text/javascript">
+function handleJs(js) {
+  js = js.replace('export default', 'return');
+  return (new Function(js))();
+}
+
+function mergeJs(js, template) {
+  const jsObj = handleJs(js);
+  const vueObj = Object.assign({template}, jsObj, {el: '#app'}); 
+  return new Vue(vueObj);
+}
+
+mergeJs(js, template);
+<\/script>`);
+      previewDoc.write('</body>');
+      previewDoc.write('</html>');
+      previewDoc.close();
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,14 @@
 <template>
   <section id="home">
     <v-row class="u-no-padding">
-      <v-col c="4" class="u-no-padding">
-        <CodePane id="html-code" v-model="templateText"></CodePane>
+      <v-col c="4" class="left u-no-padding">
+        <CodePane id="html-code" v-model="templateCode"></CodePane>
       </v-col>
-      <v-col c="4" class="u-no-padding">
-        <CodePane id="js-code" mode="javascript" v-model="jsText"></CodePane>
+      <v-col c="4" class="middle u-no-padding">
+        <CodePane id="js-code" mode="javascript" v-model="jsCode"></CodePane>
       </v-col>
-      <v-col c="4" class="u-no-padding">
-        <iframe
-          id="preview"
-          name="result"
-          sandbox="allow-forms allow-popups allow-scripts allow-same-origin"
-          frameborder="0"
-        ></iframe>
+      <v-col c="4" class="right u-no-padding">
+        <Preview :templateCode="templateCode" :jsCode="jsCode" />
       </v-col>
     </v-row>
   </section>
@@ -21,22 +16,24 @@
 
 <script>
 import CodePane from '@/components/CodePane.vue';
+import Preview from '@/components/Preview.vue';
 
 export default {
   name: 'Home',
 
   components: {
     CodePane,
+    Preview,
   },
 
   data() {
     return {
-      templateText: `<div>
+      templateCode: `<div>
   <h1>{{ count }}</h1>
   <button @click="increment">Click</button>
 </div>
 `,
-      jsText: `export default {
+      jsCode: `export default {
   name: "HelloWorld",
   data() {
     return {
@@ -51,56 +48,6 @@ export default {
 };`,
     };
   },
-
-  mounted() {
-    this.showPreview();
-  },
-
-  watch: {
-    templateText() {
-      this.showPreview();
-    },
-    jsText() {
-      this.showPreview();
-    },
-  },
-
-  methods: {
-    showPreview() {
-      const lib = 'https://cdn.jsdelivr.net/npm/vue@2.6.11';
-      const templateText = this.templateText.replace(/\s*\n+\s*/g, ' ').replace(/>\s+/g, '>').replace(/\s+</g, '<');
-      const { jsText } = this;
-      /* eslint-disable-next-line */
-      const script = 'var template = `<template>' + templateText + '</template>`;' + 'var js =`' + jsText + '`;';
-
-      const previewDoc = window.frames[0].document;
-      previewDoc.write('<!DOCTYPE html>');
-      previewDoc.write('<html>');
-      previewDoc.write('<head>');
-      previewDoc.write(`<script src=${lib} type="text/javascript"><\/script>`);
-      previewDoc.write('</head>');
-      previewDoc.write('<body>');
-      previewDoc.write('<div id="app"></div>');
-      previewDoc.write(`<script type="text/javascript">${script}<\/script>`);
-      previewDoc.write(`<script type="text/javascript">
-function handleJs(js) {
-  js = js.replace('export default', 'return');
-  return (new Function(js))();
-}
-
-function mergeJs(js, template) {
-  const jsObj = handleJs(js);
-  const vueObj = Object.assign({template}, jsObj, {el: '#app'}); 
-  return new Vue(vueObj);
-}
-
-mergeJs(js, template);
-<\/script>`);
-      previewDoc.write('</body>');
-      previewDoc.write('</html>');
-      previewDoc.close();
-    },
-  },
 };
 </script>
 
@@ -113,7 +60,7 @@ mergeJs(js, template);
     height: 100%;
   }
 
-  .col-4 {
+  .left, .middle {
     border-right: 1px solid #333;
   }
 }
