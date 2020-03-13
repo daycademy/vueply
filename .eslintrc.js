aca47734664c5fd94f73de977298f6eda97ed399
florianwoelki <florianwoelki@gmx.de>
2020-03-13 11:33:20 +0100

    fix: no-useless-escape rule option

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-useless-escape': ['error', 'never'],
+    'no-useless-escape': 'off',
   },
   overrides: [
     {
