11eccd87668938c7ae94bdb09bd89a6fc2ef20fb
florianwoelki <florianwoelki@gmx.de>
2020-03-26 19:44:43 +0100

    build: add webpack bundle analyzer plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vueply/'
     : '/',
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()],
+  },
 };
