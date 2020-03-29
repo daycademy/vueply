660d6e035ff93384357e379fbcc3a035c3995bf2
florianwoelki <florianwoelki@gmx.de>
2020-03-29 11:32:01 +0200

    feat(component): add download file button to code pane component

--- src/components/CodePane.vue
+++ src/components/CodePane.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div id="code-pane">
+    <v-btn
+      size="small"
+      color="transparent"
+      @click="download(currentFile.name, currentFile.code)"
+    >
+      <i class="fas fa-file-download"></i>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
@@ -48,6 +56,19 @@ export default class CodePane extends Vue {
     (this.$root.$refs.Preview as ThePreview).showPreview();
   }
 
+  private download = (filename: string, text: string) => {
+    const element = document.createElement('a');
+    element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
   mounted() {
     const editor = CodeMirror(this.$el as HTMLElement, {
       value: this.value,
@@ -72,3 +93,18 @@ export default class CodePane extends Vue {
   }
 }
 </script>
+
+<style lang="scss">
+#code-pane {
+  position: relative;
+
+  button {
+    z-index: 10;
+    position: absolute;
+    right: 1em;
+    top: 0.5em;
+    color: #6A6F8D;
+    font-size: 18px;
+  }
+}
+</style>
