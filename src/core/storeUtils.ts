5a55b02444a24b6263630bdf854015bb1d834ae9
florianwoelki <florianwoelki@gmx.de>
2020-04-01 11:08:43 +0200

    fix(core): set any type to RootState in Store type

--- src/core/storeUtils.ts
+++ src/core/storeUtils.ts
@@ -1,11 +1,13 @@
 import ProjectFileLink from '@/store/models/ProjectFileLink';
 import { Store } from 'vuex';
 import FileModel from '@/store/models/FileModel';
+import { RootState } from '@/store/types';
 
-export const files = (store: Store<any>, currentProject: ProjectFileLink): Array<FileModel> => store
+export const files = (store: Store<RootState>,
+  currentProject: ProjectFileLink): Array<FileModel> => store
   .getters['fileExplorer/projectFiles'](currentProject.projectName);
 
-export const currentProject = (store: Store<any>): ProjectFileLink => store
+export const currentProject = (store: Store<RootState>): ProjectFileLink => store
   .state.project.projectFilesLink
   .filter((projectFileLink: ProjectFileLink) => projectFileLink.projectName
     === store.state.project.currentProject)[0];
