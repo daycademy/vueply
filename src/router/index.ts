7026ccf875744ff9cd4807ae4781f745f616469e
florianwoelki <florianwoelki@gmx.de>
2020-04-04 10:42:28 +0200

    feat(router): add de, en routing options

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,38 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import RouterView from '../views/RouterView.vue';
 import Home from '../views/Home.vue';
+
+import i18n from '../i18n';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/:project?',
-    name: 'Home',
-    component: Home,
+    path: '/:lang',
+    component: RouterView,
+    beforeEnter(to: any, _from: any, next: any) {
+      const { lang } = to.params;
+      if (!['en', 'de'].includes(lang)) {
+        return next('de');
+      }
+
+      if (i18n.locale !== lang) {
+        i18n.locale = lang;
+      }
+      return next();
+    },
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '*',
+    redirect: '/de',
   },
 ];
 
