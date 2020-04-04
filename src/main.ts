24ac9c69278616491e9b626482b757a6fb6efd14
florianwoelki <florianwoelki@gmx.de>
2020-04-04 10:34:02 +0200

    build: add i18n dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueCirrus from 'vue-cirrus';
+import VueI18n from 'vue-i18n';
 import store from './store';
 import App from './App.vue';
 import router from './router';
+import messages from './lang';
 
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/htmlmixed/htmlmixed';
@@ -13,10 +15,18 @@ import 'vue-cirrus/dist/vue-cirrus.css';
 
 Vue.use(VueCirrus);
 
+Vue.use(VueI18n);
+export const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages,
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
