20ae55305df26f445716c8804abd8e37ce2643a4
florianwoelki <florianwoelki@gmx.de>
2020-04-05 12:20:59 +0200

    test: add sinon and test for chooseProject click

--- src/modules/FileExplorer/Tests/settingsDropdown.spec.ts
+++ src/modules/FileExplorer/Tests/settingsDropdown.spec.ts
@@ -1,12 +1,13 @@
 import VueCirrus from 'vue-cirrus';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { createLocalVue, shallowMount, Wrapper } from '@vue/test-utils';
 import SettingsDropdown from '@/modules/FileExplorer/Components/SettingsDropdown.vue';
 import ProjectFileLink from '@/store/models/ProjectFileLink';
 
 describe('FileExplorer => SettingsDropdown.vue', () => {
   let localVue;
-  let wrapper: Wrapper<SettingsDropdown>;
+  let wrapper: any;
   beforeEach(() => {
     localVue = createLocalVue();
     localVue.use(VueCirrus);
@@ -41,5 +42,11 @@ describe('FileExplorer => SettingsDropdown.vue', () => {
     /* eslint-disable-next-line */
     (wrapper.vm as any).chooseProject('javascript');
     expect(wrapper.emitted()['choose-project'][0]).eql(['javascript']);
+  });
+
+  it('chooseProject gets called on dropdown item click', async () => {
+    sinon.spy(wrapper.vm, 'chooseProject');
+    wrapper.find('v-dropdown-item-stub').vm.$emit('click');
+    expect((wrapper.vm as any).chooseProject.callCount).to.equal(1);
   });
 });
