d6d69f4d1a3a4088795b23dfb3a622bc157671e5
florianwoelki <florianwoelki@gmx.de>
2020-04-06 09:53:16 +0200

    test: add file and remove files test

---
+++ src/modules/FileExplorer/Tests/file.spec.ts
@@ -0,0 +1,32 @@
+import VueCirrus from 'vue-cirrus';
+import { expect } from 'chai';
+import { createLocalVue, shallowMount, Wrapper } from '@vue/test-utils';
+import File from '@/modules/FileExplorer/Components/File.vue';
+import i18n from '@/i18n';
+
+describe('FileExplorer => File.vue', () => {
+  let localVue;
+  let wrapper: Wrapper<File>;
+  beforeEach(() => {
+    localVue = createLocalVue();
+    localVue.use(VueCirrus);
+    wrapper = shallowMount(File, {
+      localVue,
+      i18n,
+      propsData: {
+        filename: 'index.html',
+        fileType: 'text/html',
+      },
+    });
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper.html()).to.be.a('string').and.satisfy((text: string) => text.startsWith('<p data-'));
+  });
+
+  it('emit click event', () => {
+    const button = wrapper.find('p');
+    button.vm.$emit('click');
+    return expect(wrapper.emitted().click).to.be.not.undefined;
+  });
+});
