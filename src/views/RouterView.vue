114da8d631252e4b15d486ed38b7043c14adcee1
florianwoelki <florianwoelki@gmx.de>
2020-04-07 10:21:17 +0200

    fix(view): add exported class for RouterView

--- src/views/RouterView.vue
+++ src/views/RouterView.vue
@@ -1,3 +1,11 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({})
+export default class RouterView extends Vue {}
+</script>
