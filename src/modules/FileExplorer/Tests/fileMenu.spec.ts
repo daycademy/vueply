98326fded649f9fc9a632a02f64faf41edd0e72b
florianwoelki <florianwoelki@gmx.de>
2020-04-09 12:44:08 +0200

    test: change shallowMount to mount in fileMenu test

--- src/modules/FileExplorer/Tests/fileMenu.spec.ts
+++ src/modules/FileExplorer/Tests/fileMenu.spec.ts
@@ -1,6 +1,6 @@
 import VueCirrus from 'vue-cirrus';
 import { expect } from 'chai';
-import { createLocalVue, shallowMount, Wrapper } from '@vue/test-utils';
+import { createLocalVue, Wrapper, mount } from '@vue/test-utils';
 import FileMenu from '@/modules/FileExplorer/Components/FileMenu.vue';
 import i18n from '@/i18n';
 
@@ -10,7 +10,7 @@ describe('FileExplorer => FileMenu.vue', () => {
   beforeEach(() => {
     localVue = createLocalVue();
     localVue.use(VueCirrus);
-    wrapper = shallowMount(FileMenu, { localVue, i18n });
+    wrapper = mount(FileMenu, { localVue, i18n });
   });
 
   it('renders correctly', () => {
@@ -18,13 +18,13 @@ describe('FileExplorer => FileMenu.vue', () => {
   });
 
   it('emit click download event', () => {
-    const dropdownItem = wrapper.findAll('v-dropdown-item-stub').at(0);
+    const dropdownItem = wrapper.findAll('li').at(0);
     dropdownItem.vm.$emit('click');
     return expect(wrapper.emitted()['click-download']).to.be.not.undefined;
   });
 
   it('emit click delete event', () => {
-    const dropdownItem = wrapper.findAll('v-dropdown-item-stub').at(1);
+    const dropdownItem = wrapper.findAll('li').at(1);
     dropdownItem.vm.$emit('click');
     return expect(wrapper.emitted()['click-delete']).to.be.not.undefined;
   });
