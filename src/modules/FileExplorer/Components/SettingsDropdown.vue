c10a3e09e64afc0cd6ab4ac86ffe670d4fde5296
florianwoelki <florianwoelki@gmx.de>
2020-04-29 10:55:11 +0200

    feat(module): add first new settings modal

--- src/modules/FileExplorer/Components/SettingsDropdown.vue
+++ src/modules/FileExplorer/Components/SettingsDropdown.vue
@@ -1,18 +1,35 @@
 <template>
-  <v-dropdown>
-    <template v-slot:button>
-      <v-btn size="small" color="transparent" dropdown>
+  <div class="settings-dropdown">
+    <v-btn size="small" color="transparent">
+      <a href="#settings-modal">
         <i class="icon fas fa-cog"></i>
-      </v-btn>
-    </template>
-    <v-dropdown-item
-      v-for="project in projects"
-      :key="project.projectName"
-      @click="chooseProject(project.projectName)"
-    >
-      {{ project.displayName }}
-    </v-dropdown-item>
-  </v-dropdown>
+      </a>
+    </v-btn>
+    <v-modal m-id="settings-modal" :title="$t('settings_modal_title')" zoomOut>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Explicabo ducimus dolorem excepturi facere architecto, quam commodi,
+        tenetur corrupti a eveniet laborum labore distinctio doloremque
+        eaque repellendus ab, culpa rem temporibus?
+      </p>
+    </v-modal>
+    <!--
+    <v-dropdown>
+      <template v-slot:button>
+        <v-btn size="small" color="transparent" dropdown>
+          <i class="icon fas fa-cog"></i>
+        </v-btn>
+      </template>
+      <v-dropdown-item
+        v-for="project in projects"
+        :key="project.projectName"
+        @click="chooseProject(project.projectName)"
+      >
+        {{ project.displayName }}
+      </v-dropdown-item>
+    </v-dropdown>
+    -->
+  </div>
 </template>
 
 <script lang="ts">
@@ -32,21 +49,17 @@ export default class SettingsDropdown extends Vue {
 </script>
 
 <style lang="scss">
-.list-dropdown {
+.settings-dropdown {
   margin-left: auto;
   margin-right: 7px;
 
-  .menu {
-    background-color: #282c34;
-
-    li a {
-      color: #fff;
-    }
-  }
-
   button {
-    color: #6A6F8D;
     font-size: 14px;
+    margin-bottom: 0;
+
+    a {
+      color: #6A6F8D;
+    }
   }
 }
 </style>
