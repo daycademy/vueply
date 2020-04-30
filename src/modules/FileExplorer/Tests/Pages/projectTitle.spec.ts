7b4acf310eaeb1db0dd9581f2f884527a8f10680
florianwoelki <florianwoelki@gmx.de>
2020-04-30 10:13:53 +0200

    test: fix projectTitle router

--- src/modules/FileExplorer/Tests/Pages/projectTitle.spec.ts
+++ src/modules/FileExplorer/Tests/Pages/projectTitle.spec.ts
@@ -1,9 +1,11 @@
 import VueCirrus from 'vue-cirrus';
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import { mount, Wrapper, createLocalVue } from '@vue/test-utils';
 import { expect } from 'chai';
 import i18n from '@/i18n';
 import store from '@/store';
+import router from '@/router';
 import ProjectFileLink from '@/store/models/ProjectFileLink';
 import ProjectTitle from '../../Pages/ProjectTitle.vue';
 import SettingsDropdown from '../../Components/SettingsDropdown.vue';
@@ -15,10 +17,12 @@ describe('FileExplorer => ProjectTitle', () => {
     localVue = createLocalVue();
     localVue.use(VueCirrus);
     localVue.use(Vuex);
+    localVue.use(VueRouter);
     wrapper = mount(ProjectTitle, {
       localVue,
       i18n,
       store,
+      router,
       propsData: {
         currentProject: {
           displayName: 'Test',
