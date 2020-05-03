8c36e68e4aee23cb89ad4e2037bfcb4803fb3f4a
florianwoelki <florianwoelki@gmx.de>
2020-05-03 10:55:13 +0200

    feat(translater): add basic endless loop detection

--- src/core/translater/translater.ts
+++ src/core/translater/translater.ts
@@ -39,10 +39,55 @@ const writeToDoc = (
       document.write(`<script type="text/javascript">${markdownCompiler}<\/script>`);
     }
   } else {
+    document.write(`<script>
+function limitEval(code, fnOnStop, opt_timeoutInMS) {
+  var id = Math.random() + 1,
+    blob = new Blob(
+      ['onmessage=function(a){a=a.data;postMessage({i:a.i+1});postMessage({r:eval.call(this,a.c),i:a.i})};'],
+      { type:'text/javascript' }
+    ),
+    myWorker = new Worker(URL.createObjectURL(blob));
+
+  function onDone() {
+    URL.revokeObjectURL(blob);
+    fnOnStop.apply(this, arguments);
+  }
+
+  myWorker.onmessage = function (data) {
+    data = data.data;
+    if (data) {
+      if (data.i === id) {
+        id = 0;
+        onDone(true, data.r);
+      }
+      else if (data.i === id + 1) {
+        setTimeout(function() {
+          if (id) {
+            myWorker.terminate();
+            onDone(false);
+          }
+        }, opt_timeoutInMS || 1000);
+      }
+    }
+  };
+
+  myWorker.postMessage({ c: code, i: id });
+}
+<\/script>`);
     document.write(`<script type="text/javascript">
-;(function() {
-  ${script}
-})()
+var codeStr = \`${script}\`;
+codeStr = codeStr.replace(/document.(.*?){(\\s.*)(\\s}\\);)/g, '');
+
+limitEval(codeStr, function (success) {
+  if (success) {
+    ;(function() {
+      ${script}
+    })()
+  } else {
+    console.log('Endless loop detected!');
+    throw new Error('Endless loop detected!');
+  }
+});
 <\/script>`);
   }
 
