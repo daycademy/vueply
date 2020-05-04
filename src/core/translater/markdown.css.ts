19682883c51aa094d781bf40f4aeafd301cceb40
florianwoelki <florianwoelki@gmx.de>
2020-05-04 09:15:28 +0200

    refactor(translater): move markdown css to its custom file

---
+++ src/core/translater/markdown.css.ts
@@ -0,0 +1,23 @@
+export default `@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+
+html, body {
+  font-family: 'Montserrat';
+}
+blockquote {
+  padding: 0 1em;
+  color: #6a737d;
+  border-left: 0.25em solid #dfe2e5;
+}
+table {
+  display: block;
+  width: 100%;
+  overflow: auto;
+}
+table tr {
+  background-color: #fff;
+  border-top: 1px solid #c6cbd1;
+}
+table th, table td {
+  padding: 6px 13px;
+  border: 1px solid #dfe2e5;
+}`;
--- src/core/translater/translater.ts
+++ src/core/translater/translater.ts
@@ -5,6 +5,7 @@ import tsCompiler from './tsCompiler.iframe';
 import markdownCompiler from './markdown.iframe';
 import markdownitTaskList from './markdownitTaskList';
 import endlessLoopDetector from './endlessLoopDetector';
+import markdownCss from './markdown.css';
 
 const writeToDoc = (
   document: Document,
@@ -183,31 +184,7 @@ const translateIntoTypeScript = (
 
 const translateIntoMarkdown = (frame: Window, file: string) => {
   const code = `var markdown = \`${file}\``;
-  const css = `@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-
-html, body {
-  font-family: 'Montserrat';
-}
-blockquote {
-  padding: 0 1em;
-  color: #6a737d;
-  border-left: 0.25em solid #dfe2e5;
-}
-table {
-  display: block;
-  width: 100%;
-  overflow: auto;
-}
-table tr {
-  background-color: #fff;
-  border-top: 1px solid #c6cbd1;
-}
-table th, table td {
-  padding: 6px 13px;
-  border: 1px solid #dfe2e5;
-}
-`;
-  return writeToDoc(frame.document, css, code, 'markdown');
+  return writeToDoc(frame.document, markdownCss, code, 'markdown');
 };
 
 export default {
