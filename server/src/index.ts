da934c77bb9a70f1ae76e0db88486752b26e8618
florianwoelki <florianwoelki@gmx.de>
2020-05-12 10:33:57 +0200

    feat: add bodyParser cors and dotenv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 const app = express();
-const port = 8080;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 4000);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(port, () => {
+app.listen(app.get('port'), () => {
   // tslint:disable-next-line:no-console
-  console.log(`server started at ${port}`);
+  console.log(`server started at ${app.get('port')}`);
 });
