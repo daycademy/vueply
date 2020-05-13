f09148440e87d630346afe0c5742d6fba639ba4c
florianwoelki <florianwoelki@gmx.de>
2020-05-13 18:11:54 +0200

    feat: add socketio setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,13 @@ dotenv.config({ path: '.env.local' });
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import http from 'http';
+import socketio from 'socket.io';
 
 const app = express();
+const httpServer = new http.Server(app);
+const io = socketio(httpServer);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,7 +21,12 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(app.get('port'), () => {
+io.on('connection', (socket: any) => {
+  // tslint:disable-next-line:no-console
+  console.log('a user connected');
+});
+
+const server = httpServer.listen(app.get('port'), () => {
   // tslint:disable-next-line:no-console
   console.log(`server started at ${app.get('port')}`);
 });
