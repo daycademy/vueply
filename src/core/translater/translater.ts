83fec5b9c85ace56df66f8e8f4db3e037f3c0833
florianwoelki <florianwoelki@gmx.de>
2020-05-30 13:45:52 +0200

    refactor(translater): remove endless loop detection for single files

--- src/core/translater/translater.ts
+++ src/core/translater/translater.ts
@@ -45,19 +45,9 @@ const writeToDoc = (
     }
   } else {
     document.write(`<script type="text/javascript">
-var codeStr = \`${script}\`;
-codeStr = codeStr.replace(/document.(.*?){(\\s.*)*(}\\);)/g, '');
-
-limitEval(codeStr, function (success) {
-  if (success) {
-    ;(function() {
-      ${script}
-    })()
-  } else {
-    console.log('Endless loop detected!');
-    throw new Error('Endless loop detected!');
-  }
-});
+;(function() {
+  ${script}
+})()
 <\/script>`);
   }
 
